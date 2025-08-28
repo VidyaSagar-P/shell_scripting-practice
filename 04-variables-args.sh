f6e072fb6e5df9a2a1390e68bacaf6369c38463a
vidyasagar07 <vidya07422@gmail.com>
2025-08-28 16:56:08 +0530

    variables with argunents added

---
+++ 04-variables-args.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Passing arguments for this script
+
+PERSON1=$1
+PERSON2=$2
+
+echo "${PERSON1}:: Hi $PERSON2"
+echo "${PERSON2}:: hello $PERSON1, How are you?"
+echo "${PERSON1}:: I am fine ${PERSON2}, How are you doing"
+echo "${PERSON2}:: doing great ${PERSON1}"
