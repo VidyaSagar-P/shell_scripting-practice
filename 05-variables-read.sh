b693f081ac36179cf00a9f13ac71b54d7975e433
vidyasagar07 <vidya07422@gmail.com>
2025-08-28 17:10:45 +0530

    variables with read

--- 05-variables-read.sh
+++ 05-variables-read.sh
@@ -2,10 +2,16 @@
 
 #providing username and password
 
+# echo "Please enter your name"
+# read USERNAME #it takes the user name
+# echo "User name entered:: $USERNAME"
+
+# echo "Please enter your password"
+# read -s PASSWORD #it will hides the password in commandline
+# echo "Password entered:: ${PASSWORD}"
+
+# Sample
 echo "Please enter your name"
 read USERNAME #it takes the user name
-echo "User name entered:: $USERNAME"
-
-echo "Please enter your password"
 read -s PASSWORD #it will hides the password in commandline
-echo "Password entered:: ${PASSWORD}"
+echo "You have successfully registered"
