63b130658ada129f0169029ac74676fa092eb78d
vidyasagar07 <vidya07422@gmail.com>
2025-08-28 16:52:04 +0530

    variables added

--- 03-variable.sh
+++ 03-variable.sh
@@ -4,7 +4,7 @@
 PERSON1=Vidya
 PERSON2=Angel
 
-echo "${PERSON1}:: Hi $PERSON1"
+echo "${PERSON1}:: Hi $PERSON2"
 echo "${PERSON2}:: hello $PERSON1, How are you?"
 echo "${PERSON1}:: I am fine ${PERSON2}, How are you doing"
 echo "${PERSON2}:: doing great ${PERSON1}"
