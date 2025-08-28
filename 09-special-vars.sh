00f05ff49f4ef4196a1075a8249619566c27b32d
vidyasagar07 <vidya07422@gmail.com>
2025-08-29 00:20:03 +0530

    special vars

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -3,3 +3,9 @@
 # special variables
 echo "all variables passed through the script:: $@"
 echo "number of variables/args passed:: $#"
+echo "name of the script:: $0"
+echo "current working directory:: $PWD"
+echo "home directory of current user:: $HOME"
+echo "PID of script executing now:: $$"
+sleep 100 &
+echo "PID of last background command:: $!"
