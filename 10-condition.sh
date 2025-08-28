ab591848a3a82d623b6b22bcc013473e91ba3822
vidyasagar07 <vidya07422@gmail.com>
2025-08-29 00:35:55 +0530

    simple condition

--- 10-condition.sh
+++ 10-condition.sh
@@ -4,11 +4,19 @@
 # -gt(grater than), -lt(less than), -eq(equals to), -ne(not equals), -ge(greater than or euqals),
 # -le(less than or equals)
 
-NUMBER=$1
+NUMBER1=$1
+NUMBER2=$2
 
-if [ $NUMBER -gt 20 ]
+if [ $NUMBER1 -gt 20 ]
 then
-    echo "given number $NUMBER is greater than 20"
+    echo "given number $NUMBER1 is greater than 20"
 else
-    echo "given number $NUMBER is less than 20"
+    echo "given number $NUMBER1 is less than 20"
+fi
+
+if [ $NUMBER2 -eq 20 ]
+then
+    echo "condition satisfied"
+else
+    echo "condition not satisfied"
 fi
