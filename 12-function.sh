3b2f2498944ab89d0e070cb37bc9c6bbc80f420e
vidyasagar07 <vidya07422@gmail.com>
2025-08-29 17:10:11 +0530

    functions

--- 12-function.sh
+++ 12-function.sh
@@ -2,44 +2,30 @@
 
 USERID=$(id -u)
 
-CHECK_ROOT(){
-    if [ $USERID -ne 0 ]
-    then
-        echo "Please run this script with root priveleges"
-        exit 1
-    fi
-}
+if [ $USERID -ne 0]
+then 
+    echo "Proceed with root priveleges"
+    exit 1
+fi
 
-
-VALIDATE(){
+#function
+VALIDATE (){
     if [ $1 -ne 0 ]
     then
-        echo "$2 is...FAILED"
-        exit 1
+        echo "$2 is FAILED"
     else
-        echo "$2 is...SUCCESS"
+        echo "$2 IS SUCCESS"
     fi
 }
-# ------------------------------------------------------------------>
-CHECK_ROOT
-#1
-dnf list installed git
 
+#Mysql installation
+dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo "Git is not installed, going to install it.."
-    dnf install git -y
-    VALIDATE $? "Installing Git"
+    echo "Mysql not installed, Going to install now"
+    dnf install mysql -y
+    VALIDATE $? "Mysql installing"
+    
 else
-    echo "Git is already installed, nothing to do.."
+    echo "mysql already installed"
 fi
-#2
-# dnf list installed mysql
-# if [ $? -ne 0 ]
-# then
-#     echo "MySQL is not installed...going to install"
-#     dnf install mysql -y
-#     VALIDATE $? "Installing MySQL"
-# else
-#     echo "MySQL is already installed..nothing to do"
-# fi
