f2736d88b31c171b0c8bd34019c6980d9ecbe024
vidyasagar07 <vidya07422@gmail.com>
2025-08-29 15:46:12 +0530

    installation conditions

--- 11-installation.sh
+++ 11-installation.sh
@@ -7,4 +7,25 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Proceed with the root priveleges"
+    exit 1
 fi
+
+#installing my Mysql server
+dnf list installed mysql #first checking mysql wheather it already installed or not
+
+if [ $? -ne 0 ]
+then
+    echo "Mysql is not installed. Going to install"
+    dnf install mysql -y
+    if [ $? -ne 0 ] 
+    then
+        echo "Mysql installation...FAILED"
+        exit 1
+    else
+        echo "Mysql installation...SUCCESS"
+    fi
+else
+    echo "Mysql is already Installed"
+fi   
+
+
