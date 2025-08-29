0644d8e711a5def6089ddb7118ac30eab56cd949
vidyasagar07 <vidya07422@gmail.com>
2025-08-29 16:05:40 +0530

    colours

---
+++ 13-colors.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+# condition:check for root user
+if [ $USERID -ne 0 ]
+then
+    echo "Proceed with the root priveleges"
+    exit 1
+fi
+
+#installing my Mysql server
+dnf list installed mysql #first checking mysql wheather it already installed or not
+
+if [ $? -ne 0 ]
+then
+    echo -e "$R Mysql is not installed. Going to install $N"
+    dnf install mysql -y
+    if [ $? -ne 0 ] 
+    then
+        echo "Mysql installation...FAILED"
+        exit 1
+    else
+        echo -e "$G Mysql installation...SUCCESS"
+    fi
+else
+    echo -e "Mysql is already $G Installed $N"
+fi   
+
+
