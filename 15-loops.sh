e37a2c39febde9b88bb1c252615a1ea7758ddfbb
vidyasagar07 <vidya07422@gmail.com>
2025-08-30 11:11:17 +0530

    loops

--- 15-loops.sh
+++ 15-loops.sh
@@ -14,7 +14,7 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then
     echo "mysql not installed, Going to install it"
-    dnf install mysqls -y
+    dnf install mysql -y
     if [ $? -ne 0 ]
     then
         echo "there is an error installing mysql, please check the script"
