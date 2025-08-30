5b53a02b218efff3b4ce4ce1f118bfa62a726c48
vidyasagar07 <vidya07422@gmail.com>
2025-08-30 11:05:29 +0530

    loops

--- 15-loops.sh
+++ 15-loops.sh
@@ -5,4 +5,23 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Proceed with ROOT priveleges"
+    exit 1
 fi
+
+# installing packages
+dnf list installed mysql
+
+if [ $? -ne 0]
+then
+    echo "mysql not installed, Going to install it"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "there is an error installing mysql, please check the script"
+        exit 1
+    else
+        echo "mysql installation..SUCCESS"
+else
+    echo "mysql already installed"
+fi
+
